48e2ceb53e65fa446cba4a021119c13021655613
S1Lazza <lazzaroni.stefano93@gmail.com>
2024-04-05 14:17:57 +0200

    add: Gauntlet dependency

--- Source/GauntletDemo/GauntletDemo.Build.cs
+++ Source/GauntletDemo/GauntletDemo.Build.cs
@@ -10,7 +10,7 @@ public class GauntletDemo : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "Gauntlet" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
